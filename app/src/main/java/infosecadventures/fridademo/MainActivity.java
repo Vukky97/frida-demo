e056122d9a05be36abfa0a022a094fb6435d91f0
Tóth Kristóf <tothkrisi@gmail.com>
2018-11-10 13:33:46 +0100

    Add fragment navigation

--- app/src/main/java/infosecadventures/fridademo/MainActivity.java
+++ app/src/main/java/infosecadventures/fridademo/MainActivity.java
@@ -5,40 +5,40 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import infosecadventures.fridademo.fragments.PinBypass;
+import infosecadventures.fridademo.fragments.RootBypass;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frame, new PinBypass())
+                                .commit();
+                        return true;
+                    case R.id.navigation_dashboard:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frame, new RootBypass())
+                                .commit();
+                        return true;
+                    case R.id.navigation_notifications:
+                        return true;
+                }
+                return false;
+            }
+        });
+        navigation.setSelectedItemId(R.id.navigation_home);
     }
-
 }
