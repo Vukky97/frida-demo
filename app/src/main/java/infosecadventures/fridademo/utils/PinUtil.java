a07dcac370cfe8ab92938611eb1c3d3b6a6d07a6
Tóth Kristóf <tothkrisi@gmail.com>
2018-11-10 13:34:26 +0100

    Add util classes for root and pin check

---
+++ app/src/main/java/infosecadventures/fridademo/utils/PinUtil.java
@@ -0,0 +1,8 @@
+package infosecadventures.fridademo.utils;
+
+public class PinUtil {
+
+    public static boolean checkPin(int pin) {
+        return pin == 4863;
+    }
+}
---
+++ app/src/main/java/infosecadventures/fridademo/utils/RootUtil.java
@@ -0,0 +1,28 @@
+package infosecadventures.fridademo.utils;
+
+import java.io.File;
+
+public class RootUtil {
+
+    public static boolean isRootAvailable() {
+        String[] paths = {"/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
+                "/system/bin/failsafe/su", "/data/local/su", "/su/bin/su"};
+        for (String path : paths) {
+            if (new File(path).exists()) return true;
+        }
+        return false;
+    }
+
+    /*private static boolean isRootAvailable() {
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(new String[]{"/system/xbin/which", "su"});
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            return in.readLine() != null;
+        } catch (Throwable t) {
+            return false;
+        } finally {
+            if (process != null) process.destroy();
+        }
+    }*/
+}
