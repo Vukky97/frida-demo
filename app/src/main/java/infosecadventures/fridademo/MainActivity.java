59dc1fb9abd1abaafa0a7d62d352e9d15287925d
Tóth Kristóf <tothkrisi@gmail.com>
2018-11-10 22:42:06 +0100

    Menu item id refactor

--- app/src/main/java/infosecadventures/fridademo/MainActivity.java
+++ app/src/main/java/infosecadventures/fridademo/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import infosecadventures.fridademo.fragments.EncryptionKey;
 import infosecadventures.fridademo.fragments.PinBypass;
 import infosecadventures.fridademo.fragments.RootBypass;
 
@@ -21,24 +22,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
-                    case R.id.navigation_home:
+                    case R.id.pin_bypass:
                         getSupportFragmentManager()
                                 .beginTransaction()
                                 .replace(R.id.frame, new PinBypass())
                                 .commit();
                         return true;
-                    case R.id.navigation_dashboard:
+                    case R.id.root_bypass:
                         getSupportFragmentManager()
                                 .beginTransaction()
                                 .replace(R.id.frame, new RootBypass())
                                 .commit();
                         return true;
-                    case R.id.navigation_notifications:
+                    case R.id.encryption_key:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frame, new EncryptionKey())
+                                .commit();
                         return true;
                 }
                 return false;
             }
         });
-        navigation.setSelectedItemId(R.id.navigation_home);
+        navigation.setSelectedItemId(R.id.pin_bypass);
     }
 }
