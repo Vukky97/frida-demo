bc76cde3d5e1ce17b0ede153669ee70205966b30
Tóth Kristóf <tothkrisi@gmail.com>
2018-11-12 16:04:34 +0100

    Code refactor

--- app/src/main/java/infosecadventures/fridademo/utils/PinUtil.java
+++ app/src/main/java/infosecadventures/fridademo/utils/PinUtil.java
@@ -1,12 +1,10 @@
 package infosecadventures.fridademo.utils;
 
-import android.content.Context;
-
-import infosecadventures.fridademo.R;
+import android.util.Base64;
 
 public class PinUtil {
 
-    public static boolean checkPin(Context context, String pin) {
-        return pin.equals(context.getString(R.string.pin));
+    public static boolean checkPin(String pin) {
+        return pin.equals(new String(Base64.decode("NDg2Mw==", Base64.DEFAULT)));
     }
 }
